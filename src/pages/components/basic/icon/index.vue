5390a1107076f9dd30a6e3a3e05fc915814193ed
hhq <honghq@weipaitang.com>
2021-06-03 21:03:20 +0800

    mod: icon图标字体

--- src/pages/components/basic/icon/index.vue
+++ src/pages/components/basic/icon/index.vue
@@ -1,16 +1,46 @@
 <template>
-  <view class="icon-page wrap-page">
-    <view class="iconfont">&#xe65e;</view>
-    <view class="iconfont icon-unfold"></view>
+  <view class="wrap-page">
+    <view class="icon-title">记得前面加上 icon-xxx</view>
+    <u-grid :col="3">
+      <u-grid-item v-for="(icon, index) in icons.glyphs" :key="icon.icon_id" :index="index+1" >
+        <view :class="['iconfont', `${icons.css_prefix_text}${icon.font_class}`]" :style="{fontSize: '50rpx'}"></view>
+        <view class="u-m-t-20">{{ `${icon.font_class}` }}</view>
+      </u-grid-item>
+    </u-grid>
   </view>
 </template>
 
 <script>
+import icons from '@/u-ui/font/icon.json'
 export default {
-  name: "IconPage"
+  name: "IconPage",
+  data() {
+    return {
+      icons
+    }
+  }
 }
 </script>
 
-<style scoped>
+<style scoped lang="scss">
+.icon-title {
+  margin-bottom: 30rpx;
+  color: coral;
+}
+.icon-container {
+  display: flex;
+  flex-wrap: wrap;
 
+  .icon-item {
+    width: 33%;
+    display: flex;
+    flex-direction: column;
+    justify-self: center;
+    align-items: center;
+    margin-bottom: 20rpx;
+    .iconfont {
+      font-size: 40rpx;
+    }
+  }
+}
 </style>
